95852c59b2f447bc6d837b196edeb5fe8e20b4e2
Marco Ancona <marco.ancona@inf.ethz.ch>
2017-11-02 23:36:59 +0100

    WIP: tests grad

--- deepexplain/tests/test_tensorflow.py
+++ deepexplain/tests/test_tensorflow.py
@@ -4,18 +4,58 @@ import logging
 import tensorflow as tf
 import numpy as np
 
+tf.set_random_seed(10)
+np.random.seed(10)
 
-def tf_simple_model(activation, session):
+def simple_model(activation, session):
     X = tf.placeholder("float", [None, 2])
-    w1 = tf.Variable(initial_value=[[1.0, 1.0], [1.0, 1.0]])
-    b1 = tf.Variable(initial_value=[0.0, 0.0])
-    w2 = tf.Variable(initial_value=[[1.0, 1.0], [1.0, 1.0]])
-    b2 = tf.Variable(initial_value=[0.0, 0.0])
+    w1 = tf.Variable(initial_value=[[1.0, -1.0], [-1.0, 1.0]])
+    b1 = tf.Variable(initial_value=[1.5, -1.0])
+    w2 = tf.Variable(initial_value=[[1.1, 1.4], [-0.5, 1.0]])
+    b2 = tf.Variable(initial_value=[0.0, 2.0])
 
     layer1 = activation(tf.matmul(X, w1) + b1)
     out = tf.matmul(layer1, w2) + b2
+    session.run(tf.global_variables_initializer())
+    return X, out
+
+
+def simpler_model(activation, session):
+    X = tf.placeholder("float", [None, 2])
+    w1 = tf.Variable(initial_value=[[1.0, -1.0], [-1.0, 1.0]])
+    b1 = tf.Variable(initial_value=[0.5, -0.5])
+
+    out = activation(tf.matmul(X, w1) + b1)
     session.run(tf.initialize_all_variables())
     return X, out
+
+
+def train_xor(session):
+    tf.set_random_seed(10)
+    np.random.seed(10)
+    X = tf.placeholder("float", [None, 2])
+    Y = tf.placeholder("float", [None, 1])
+    w1 = tf.Variable(tf.random_normal([2, 2]))
+    b1 = tf.Variable(tf.random_normal([2]))
+    w2 = tf.Variable(tf.random_normal([2, 1]))
+    b2 = tf.Variable(tf.random_normal([1]))
+
+    l1 = tf.nn.relu(tf.matmul(X, w1) + b1)
+    out = tf.nn.relu(tf.matmul(l1, w2) + b2)
+    session.run(tf.global_variables_initializer())
+
+    # Define loss and optimizer
+    loss = tf.reduce_mean(tf.losses.mean_squared_error(Y, out))
+    logging.critical(loss)
+    train_step = tf.train.GradientDescentOptimizer(0.05).minimize(loss)
+
+    # Generate dataset random
+    x = np.random.randint(0, 2, size=(10, 2))
+    y = np.expand_dims(np.logical_or(x[:, 0], x[:, 1]), -1)
+    l = None
+    for _ in range(100):
+        l, _, = session.run([loss, train_step], feed_dict={X: x, Y: y})
+    return np.abs(l - 0.1) < 0.01
 
 
 class TestDeepExplainTF(TestCase):
@@ -30,11 +70,33 @@ class TestDeepExplainTF(TestCase):
         except Exception:
             self.fail("Tensorflow requirement not met")
 
-    def test_simple_tf_model(self):
+    def test_simple_model(self):
         session = tf.Session()
-        X, out = tf_simple_model(tf.nn.relu, session)
-        xi = np.array([[1,1]])
+        X, out = simple_model(tf.nn.relu, session)
+        xi = np.array([[1, 0]])
         r = session.run(out, {X: xi})
         self.assertEqual(r.shape, xi.shape)
-        np.testing.assert_equal(r[0], [4.0, 4.0])
+        np.testing.assert_equal(r[0], [2.75,  5.5])
 
+    def test_training(self):
+        session = tf.Session()
+        r = train_xor(session)
+        self.assertTrue(r)
+
+    def test_dummy_zero(self):
+        pass
+
+    def test_saliency_maps(self):
+        pass
+
+    def test_gradient_x_input(self):
+        pass
+
+    def test_deep_lift(self):
+        pass
+
+    def test_elrp(self):
+        pass
+
+    def test_integrated_gradients(self):
+        pass
